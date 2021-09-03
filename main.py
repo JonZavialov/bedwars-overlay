03ae8b7f163284a8074352b8869ce08104f72762
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-09-02 22:01:05 -0400

    added init page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utilities import checkForNewLines
+from utilities import openBrowser
 from threading import Thread
 from flask import *
 from jsonMethods import write
@@ -19,5 +20,10 @@ def data():
 def home():
     return render_template('index.html')
 
+@app.route("/init")
+def init():
+    return render_template('init.html')
+
 if __name__ == "__main__":
+    openBrowser.openBrowser("http://localhost:5000/init")
     app.run(host='localhost')
---
+++ utilities/openBrowser.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+def openBrowser(url):
+    webbrowser.open(url, new=0, autoraise=True)
